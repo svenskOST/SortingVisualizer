fafbdc82cbdce8faa5454d692d8cab5d06bb5326
Alexander Marini <svenskost6@gmail.com>
2024-02-06 23:51:23 +0100

    Works no blinking

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace SortingVisualizer
 
         private void InitializeData()
         {
-            data = new int[100];
+            data = new int[10];
             stapleWidth = VisualizerCanvas.ActualWidth / data.Length;
             Random rand = new();
 
@@ -57,10 +57,87 @@ namespace SortingVisualizer
             }
         }
 
+        private void Sort()
+        {
+            switch (AlgorithmComboBox.Text)
+            {
+                case "Quicksort":
+                    Quicksort();
+                    break;
+                case "Bubble sort":
+                    BubbleSort();
+                    break;
+                default:
+                    MessageBox.Show("Please select an algorithm");
+                    break;
+            }
+        }
+
+        private async void Quicksort()
+        {
+            Stack<int> stack = new();
+            stack.Push(0);
+            stack.Push(data!.Length - 1);
+
+            while (stack.Count > 0)
+            {
+                int high = stack.Pop();
+                int low = stack.Pop();
+
+                if (low < high)
+                {
+                    int partitionIndex = Partition(low, high);
+
+                    stack.Push(low);
+                    stack.Push(partitionIndex - 1);
+
+                    stack.Push(partitionIndex + 1);
+                    stack.Push(high);
+
+                    await Task.Delay(speed);
+                    UpdateCanvas();
+                }
+            }
+
+            static int Partition(int low, int high)
+            {
+                int pivot = data![high];
+                int a = low - 1;
+
+                for (int b = low; b <= high; b++)
+                {
+                    if (data[b] < pivot)
+                    {
+                        a++;
+                        (data[b], data[a]) = (data[a], data[b]);
+                    }
+                }
+
+                (data[high], data[a + 1]) = (data[a + 1], data[high]);
+
+                return a + 1;
+            }
+        }
+
+        private void BubbleSort()
+        {
+
+        }
+
         private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
         {
             InitializeData();
             ClipCanvas();
+        }
+
+        private void OnSortClick(object sender, RoutedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void OnResetClick(object sender, RoutedEventArgs e)
+        {
+            InitializeData();
         }
     }
 }
